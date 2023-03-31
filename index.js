aa202e1a747f3be5b474a7b900aa155a8fcf33cc
famipiji <fahmihafizi40@gmail.com>
2023-03-31 09:52:58 +0800

    login and register system

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let friends = [
+let dbUsers = [
     {
         username: "soo",
         phone: "123142",
@@ -16,7 +16,7 @@ let friends = [
 
 function login(username, email){
     console.log("someone try to login with", username, email)
-    let matched = friends.find(Element => 
+    let matched = dbUsers.find(Element => 
         //console.log(Element)
         Element.username == username
     )//finding element in the array
@@ -31,9 +31,25 @@ if(matched){
 }
 }
 
+function register(newusername, newphone, newemail){
+{
+    dbUsers.push({
+        username: newusername,
+        phone: newphone,
+        email: newemail
+    })
+}
+}
+
 //try to login
 //login("soo","email")
 //login("daus","email")
 
-console.log( login("daus", "dauskepam@gmail.com"))//<--found
-console.log(login("deyy","nclub@gmail.com"))//<---not found sebab error
+//console.log( login("daus", "dauskepam@gmail.com"))//<--found
+//console.log(login("deyy","nclub@gmail.com"))//<---not found sebab error
+register("mader", "666", "madecod@gmail.com")
+register("ahwong", "619", "ahwongar@gmail.com")
+//let matched = dbUsers.find(Element => 
+//console.log(Element))
+console.log(login("mader","madecod@gmail.com"))
+console.log(login("ahwong", "ahwongar@gmail.com"))
